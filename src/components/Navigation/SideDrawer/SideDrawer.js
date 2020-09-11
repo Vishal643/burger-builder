1245586113ec43b74eb3b24b8b168fcd0f3c48ed
Vishal Kumar <vishalkumar199812@gmail.com>
2020-09-11 11:03:17 +0530

    made changes due to rename aux file to _aux file

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,26 +4,26 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux/Aux';
+import Aux from '../../../hoc/_Aux/_Aux';
 
 const sideDrawer = (props) => {
-  let attachedstyles = [styles.SideDrawer, styles.Close];
-  if (props.open) {
-    attachedstyles = [styles.SideDrawer, styles.Open];
-  }
-  return (
-    <Aux>
-      <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachedstyles.join(' ')}>
-        <div className={styles.Logo}>
-          <Logo />
-        </div>
-        <nav>
-          <NavigationItems />
-        </nav>
-      </div>
-    </Aux>
-  );
+	let attachedstyles = [styles.SideDrawer, styles.Close];
+	if (props.open) {
+		attachedstyles = [styles.SideDrawer, styles.Open];
+	}
+	return (
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div className={attachedstyles.join(' ')}>
+				<div className={styles.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
+	);
 };
 
 export default sideDrawer;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react';
 
-import Aux from '../Aux/Aux';
+import Aux from '../_Aux/_Aux';
 import styles from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-  state = {
-    showSideDrawer: false,
-  };
+	state = {
+		showSideDrawer: false,
+	};
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
-  };
+	sideDrawerClosedHandler = () => {
+		this.setState({ showSideDrawer: false });
+	};
 
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return { showSideDrawer: !prevState.showSideDrawer };
-    });
-  };
+	sideDrawerToggleHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !prevState.showSideDrawer };
+		});
+	};
 
-  render() {
-    return (
-      <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer
-          open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
-        <main className={styles.Content}>{this.props.children}</main>
-      </Aux>
-    );
-  }
+	render() {
+		return (
+			<Aux>
+				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+				<SideDrawer
+					open={this.state.showSideDrawer}
+					closed={this.sideDrawerClosedHandler}
+				/>
+				<main className={styles.Content}>{this.props.children}</main>
+			</Aux>
+		);
+	}
 }
 
 export default Layout;
